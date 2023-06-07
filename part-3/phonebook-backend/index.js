b0fee7bc64be9223c78192b2cad7768c9bcbc71c
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2023-06-07 16:55:38 +0300

    uibuild

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -16,10 +16,6 @@ app.use(
 )
 app.use(cors())
 app.use(express.static('build'))
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
   
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
